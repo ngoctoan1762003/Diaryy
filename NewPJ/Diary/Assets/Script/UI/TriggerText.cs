1c063bdbe346b7e6c704814e3c3f630c2340bba5
ngoctoan1762003 <54841155+ngoctoan1762003@users.noreply.github.com>
2022-03-07 08:56:51 +0700

    Adjust

--- NewPJ/Diary/Assets/Script/UI/TriggerText.cs
+++ NewPJ/Diary/Assets/Script/UI/TriggerText.cs
@@ -9,7 +9,7 @@ public class TriggerText : MonoBehaviour
     public string Text;
 
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnTriggerStay2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
